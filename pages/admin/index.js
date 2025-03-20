6e81f3169992426b3c67049e614327f5241c3bf1
Everton Nascimento <seu.email@exemplo.com>
2025-03-19 22:51:30 -0300

    Corrigido links para páginas inexistentes no painel administrativo

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -204,23 +204,12 @@ export default function AdminDashboard() {
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-2" viewBox="0 0 20 20" fill="currentColor">
                   <path d="M9 6a3 3 0 11-6 0 3 3 0 016 0zM17 6a3 3 0 11-6 0 3 3 0 016 0zM12.93 17c.046-.327.07-.66.07-1a6.97 6.97 0 00-1.5-4.33A5 5 0 0119 16v1h-6.07zM6 11a5 5 0 015 5v1H1v-1a5 5 0 015-5z" />
                 </svg>
-                Lista de Usuários
+                Gerenciar Usuários
               </Link>
             </li>
             <li>
               <Link 
-                href="/admin/users/new" 
-                className="text-blue-600 hover:text-blue-800 flex items-center"
-              >
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-2" viewBox="0 0 20 20" fill="currentColor">
-                  <path d="M8 9a3 3 0 100-6 3 3 0 000 6zM8 11a6 6 0 016 6H2a6 6 0 016-6zM16 7a1 1 0 10-2 0v1h-1a1 1 0 100 2h1v1a1 1 0 102 0v-1h1a1 1 0 100-2h-1V7z" />
-                </svg>
-                Adicionar Novo Usuário
-              </Link>
-            </li>
-            <li>
-              <Link 
-                href="/admin/trial-periods" 
+                href="/admin/settings" 
                 className="text-blue-600 hover:text-blue-800 flex items-center"
               >
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-2" viewBox="0 0 20 20" fill="currentColor">
@@ -244,17 +233,6 @@ export default function AdminDashboard() {
                   <path fillRule="evenodd" d="M18 5v8a2 2 0 01-2 2h-5l-5 4v-4H4a2 2 0 01-2-2V5a2 2 0 012-2h12a2 2 0 012 2zM7 8H5v2h2V8zm2 0h2v2H9V8zm6 0h-2v2h2V8z" clipRule="evenodd" />
                 </svg>
                 Gerenciar Feedbacks
-              </Link>
-            </li>
-            <li>
-              <Link 
-                href="/admin/announcements" 
-                className="text-blue-600 hover:text-blue-800 flex items-center"
-              >
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-2" viewBox="0 0 20 20" fill="currentColor">
-                  <path fillRule="evenodd" d="M18 3a1 1 0 00-1.447-.894L8.763 6H5a3 3 0 000 6h.28l1.771 5.316A1 1 0 008 18h1a1 1 0 001-1v-4.382l6.553 3.276A1 1 0 0018 15V3z" clipRule="evenodd" />
-                </svg>
-                Criar Anúncio
               </Link>
             </li>
           </ul>
