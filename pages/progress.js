d8629475a490c15dace2eb6a18e93e908d5a17c4
Everton Nascimento <seu.email@exemplo.com>
2025-03-25 06:02:24 -0300

    Atualiza página de progresso com layout básico temporário

--- pages/progress.js
+++ pages/progress.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import ProgressReport from '../components/ProgressReport';
+import Layout from '../components/Layout';
 
 const ProgressPage = () => {
     return (
-        <div>
-            <ProgressReport />
-        </div>
+        <Layout title="Progresso">
+            <div className="container mx-auto px-4 py-8">
+                <h1 className="text-2xl font-bold mb-4">Progresso</h1>
+                <p>Em desenvolvimento...</p>
+            </div>
+        </Layout>
     );
 };
 
