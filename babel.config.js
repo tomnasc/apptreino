4bdb368c0bd6323b4c16093d3655e8466ab8eb24
Everton Nascimento <seu.email@exemplo.com>
2025-04-21 02:50:26 -0300

    Adicionar configuração do Babel e dependências necessárias para o build

---
+++ babel.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  presets: [
+    [
+      'next/babel',
+      {
+        'preset-react': {
+          runtime: 'automatic',
+          importSource: 'react'
+        }
+      }
+    ]
+  ],
+  plugins: []
+}; 
